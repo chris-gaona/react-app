53896ad6ba21bae730e0150905f2066028de9a71
Chris Gaona <thebeloved88@sbcglobal.net>
2017-01-28 17:00:24 -0800

    Adds first state to component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 class App extends React.Component {
-    render() {
-        return <h1>Hello World {this.props.txt}</h1>;
+    constructor() {
+        super();
+        this.state = {
+            txt: 'this is the state text'
+        }
     }
+
+    _update(e) {
+        this.setState({
+            txt: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" onChange={this._update.bind(this)} />
+                <h1>Hello World {this.state.txt}</h1>
+            </div>
+        );
+    };
 }
 
 App.propTypes = {

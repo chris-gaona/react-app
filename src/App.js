e7fa4686b0cdfa12fc25118dba6ca41309389405
Chris Gaona <thebeloved88@sbcglobal.net>
2017-01-28 17:16:42 -0800

    Adds custom validation to props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// main class
 class App extends React.Component {
     constructor() {
         super();
@@ -19,28 +20,44 @@ class App extends React.Component {
             <div>
                 <h1>Hello World {this.state.txt}</h1>
                 <Widget update={this._update.bind(this)}/>
-                <Button>I <Heart /> React</Button>
+                <Button>I <Heart text="Hello kid" /> React</Button>
             </div>
         );
     };
 }
 
+// stateless component
 const Button = (props) => <button>{props.children}</button>;
 
+// class
 class Heart extends React.Component {
     render() {
       return <span>&hearts;</span>
     };
 }
 
+// stateless component
 const Widget = (props) =>
     <input type="text" onChange={props.update} />;
 
+// prop validation
 App.propTypes = {
     txt: React.PropTypes.string.isRequired,
     cat: React.PropTypes.number.isRequired
 };
 
+Heart.propTypes = {
+    text(props, propName, component) {
+        if (!(propName in props)) {
+            return new Error(`missing ${propName}`);
+        }
+        if (props[propName].length < 20) {
+            return new Error(`${propName} was too short`);
+        }
+    }
+};
+
+// set default prop text
 App.defaultProps = {
     txt: "this is the default text"
 };
